ce1e5321e7bf70c3abff16ab5423bf06222e6d46
Mitchell Resnick <mitchellresnick@users.noreply.github.com>
2018-07-02 09:50:47 -0400

    Feature update
    Added new features:
    - Automatic error handling
    - Multiple category support

--- sitereview.py
+++ sitereview.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 import json
 import requests
 import sys
+import time
 
 class SiteReview(object):
     def __init__(self):
@@ -29,29 +30,44 @@ class SiteReview(object):
 
     def check_response(self, response):
         if self.req.status_code != 200:
-            sys.exit("[-] HTTP {} returned".format(req.status_code))
+            sys.exit("[-] HTTP {} Returned".format(req.status_code))
         else:
             self.category = response["categorization"][0]["name"]
             self.date = response["translatedRateDates"][0]["text"][0:35]
             self.url = response["url"]
+            try:
+                self.category2 = response["categorization"][1]["name"]
+            except IndexError:
+                pass
+            
 
 
 
 def main():
 
     f = open("url.txt")
+    o = open("output.txt", 'a+')
 
     for url in f:
+        time.sleep(5)
         s = SiteReview()
         response = s.sitereview(url)
-        s.check_response(response)
-        print(s.url + " , " + s.category)
+        try:
+            s.check_response(response)
+        except NameError:
+            print("URL FORMAT ERROR")
+            o.write("URL FORMAT ERROR" + "\n")
+            continue
+        try:
+            print(s.url + "," + s.category + "," + s.category2)
+            o.write(s.url + "," + s.category + "," + s.category2 + "\n")
+        except AttributeError:
+            print(s.url + "," + s.category)
+            o.write(s.url + "," + s.category + "\n")
 
 
+    f.close()
+    o.close()
+    
 if __name__ == "__main__":
     main()
-    # p = ArgumentParser()
-    # p.add_argument("url", help="Submit domain/URL to Symantec's Site Review")
-    # args = p.parse_args()
-
-    # main(args.url)
